cc28a2f769a758ffd136e27dceb270cd566d9f17
Shivank Gupta <shivank.gupta@adypu.edu.in>
2026-04-17 20:47:26 +0530

    Secure: Restrict CORS to CLIENT_URL

--- server/app.js
+++ server/app.js
@@ -12,12 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      process.env.CLIENT_URL,
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      "http://51.20.138.119:5173",
-    ].filter(Boolean), // This prevents errors if CLIENT_URL is undefined
+    origin: [process.env.CLIENT_URL].filter(Boolean),
     credentials: true,
   }),
 );
